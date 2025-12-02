8fe2b46ee5bae2488a836e3f30690b074f77393e
safiullah sediqi <152120211031@ogrenci.ogu.edu.tr>
2025-12-02 16:26:55 +0300

    updated

--- Board_1/keypad.asm
+++ Board_1/keypad.asm
@@ -5,9 +5,7 @@
         PSECT keypad_code, class=CODE, delta=2
 
 INIT_Keypad:
-        ; (Gerekli donan?m yoksa bo? b?rak?yoruz)
         return
 
-Keypad_Interrupt_Handler:
-        ; RB port de?i?imi vs. i?lemleri burada olurdu
+Keypad_Interrupt_Handler:  
         return
