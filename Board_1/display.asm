faf611ff1206f88b32706effe933c45b0d40ab1c
safiullah sediqi <152120211031@ogrenci.ogu.edu.tr>
2025-12-15 19:25:40 +0300

    display updated

--- Board_1/display.asm
+++ Board_1/display.asm
@@ -1,8 +1,12 @@
-; ======================================================
-; MODULE: display.asm  (BANK3 udata, iskelet)
-; ======================================================
-
-        PSECT display_code, class=CODE, delta=2
+        processor 16F877A
+        #include <xc.inc>
+	
+	
+	GLOBAL INIT_Display
+	GLOBAL Display_Multiplex_Routine
+	
+	
+        psect display_code, class=CODE
 
 INIT_Display:
         return
