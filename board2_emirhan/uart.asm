138062b4f00e8422cc3c6f4ef717f55997563bbc
mertiodas <mertcengiz2100@gmail.com>
2025-12-28 23:22:07 +0300

    updated

--- board2_emirhan/uart.asm
+++ board2_emirhan/uart.asm
@@ -2,6 +2,7 @@
 ; uart.asm - Board 2 UART Driver & Command Parser
 ; Baud Rate: 9600 @ 4MHz (SPBRG=25, BRGH=1)
 ; Handles Curtain, Temp, Pressure, Light commands.
+; Author: Mert Cengiz, aka mertiodas
 ; ============================================
 
 ; --------------------------------------------
@@ -47,91 +48,74 @@ TX_WAIT:
 ; Main polling loop for communication
 ; --------------------------------------------
 UART_SERVICE:
-    ; Check for incoming data
-    BTFSS   PIR1, 5         ; RCIF set? if=1, full
+    BTFSS PIR1,5        ; RCIF? No data
     RETURN
 
-    ; Check Errors
-    BTFSC   RCSTA, 1        ; OERR?
-    GOTO    UART_ERR_OERR
-    BTFSC   RCSTA, 2        ; FERR?
-    GOTO    UART_ERR_FERR
+    ; Check for errors
+    BTFSC RCSTA,1       ; OERR?
+    GOTO UART_ERR_OERR
+    BTFSC RCSTA,2       ; FERR?
+    GOTO UART_ERR_FERR
 
-    ; Read Data
-    MOVF    RCREG, W
-    MOVWF   UART_RX_BYTE
+    ; Read incoming byte
+    MOVF RCREG,W
+    MOVWF UART_RX_BYTE
 
-    ; ========================================
-    ; BOARD 2 COMMAND PARSER
-    ; Source: Project PDF Page 19 Table
-    ; ========================================
+    ; --- SET Commands ---
+    ; 10xxxxxx = Fractional
+    MOVF UART_RX_BYTE,W
+    ANDLW 0xC0
+    XORLW 0x80
+    BTFSC STATUS,2
+    GOTO CMD_SET_CURTAIN_LOW
 
-    ; --- SET COMMANDS (Curtain Control) ---
-    ; Format: 10xxxxxx (Low/Frac) or 11xxxxxx (High/Int)
+    ; 11xxxxxx = Integral
+    MOVF UART_RX_BYTE,W
+    ANDLW 0xC0
+    XORLW 0xC0
+    BTFSC STATUS,2
+    GOTO CMD_SET_CURTAIN_HIGH
 
-    ; Check SET CURTAIN LOW (10xxxxxx)
-    MOVF    UART_RX_BYTE, W
-    ANDLW   0xC0	    ; 1100 0000
-    XORLW   0x80            ; 1000 0000
-    BTFSC   STATUS, 2
-    GOTO    CMD_SET_CURTAIN_LOW
+    ; --- GET Commands ---
+    MOVF UART_RX_BYTE,W
+    XORLW 0x01
+    BTFSC STATUS,2
+    GOTO CMD_GET_CURTAIN_LOW
 
-    ; Check SET CURTAIN HIGH (11xxxxxx)
-    MOVF    UART_RX_BYTE, W
-    ANDLW   0xC0
-    XORLW   0xC0            ; 11000000?
-    BTFSC   STATUS, 2
-    GOTO    CMD_SET_CURTAIN_HIGH
+    MOVF UART_RX_BYTE,W
+    XORLW 0x02
+    BTFSC STATUS,2
+    GOTO CMD_GET_CURTAIN_HIGH
 
-    ; --- GET COMMANDS (Sensors & Status) ---
+    MOVF UART_RX_BYTE,W
+    XORLW 0x03
+    BTFSC STATUS,2
+    GOTO CMD_GET_TEMP_LOW
 
-    ; 0x01: Get Curtain Low (Fractional)
-    MOVF    UART_RX_BYTE, W
-    XORLW   0x01
-    BTFSC   STATUS, 2
-    GOTO    CMD_GET_CURTAIN_LOW
+    MOVF UART_RX_BYTE,W
+    XORLW 0x04
+    BTFSC STATUS,2
+    GOTO CMD_GET_TEMP_HIGH
 
-    ; 0x02: Get Curtain High (Integral)
-    MOVF    UART_RX_BYTE, W
-    XORLW   0x02
-    BTFSC   STATUS, 2
-    GOTO    CMD_GET_CURTAIN_HIGH
+    MOVF UART_RX_BYTE,W
+    XORLW 0x05
+    BTFSC STATUS,2
+    GOTO CMD_GET_PRESS_LOW
 
-    ; 0x03: Get Outdoor Temp Low
-    MOVF    UART_RX_BYTE, W
-    XORLW   0x03
-    BTFSC   STATUS, 2
-    GOTO    CMD_GET_TEMP_LOW
+    MOVF UART_RX_BYTE,W
+    XORLW 0x06
+    BTFSC STATUS,2
+    GOTO CMD_GET_PRESS_HIGH
 
-    ; 0x04: Get Outdoor Temp High
-    MOVF    UART_RX_BYTE, W
-    XORLW   0x04
-    BTFSC   STATUS, 2
-    GOTO    CMD_GET_TEMP_HIGH
+    MOVF UART_RX_BYTE,W
+    XORLW 0x07
+    BTFSC STATUS,2
+    GOTO CMD_GET_LIGHT_LOW
 
-    ; 0x05: Get Outdoor Pressure Low
-    MOVF    UART_RX_BYTE, W
-    XORLW   0x05
-    BTFSC   STATUS, 2
-    GOTO    CMD_GET_PRESS_LOW
-
-    ; 0x06: Get Outdoor Pressure High
-    MOVF    UART_RX_BYTE, W
-    XORLW   0x06
-    BTFSC   STATUS, 2
-    GOTO    CMD_GET_PRESS_HIGH
-
-    ; 0x07: Get Light Intensity Low
-    MOVF    UART_RX_BYTE, W
-    XORLW   0x07
-    BTFSC   STATUS, 2
-    GOTO    CMD_GET_LIGHT_LOW
-
-    ; 0x08: Get Light Intensity High
-    MOVF    UART_RX_BYTE, W
-    XORLW   0x08
-    BTFSC   STATUS, 2
-    GOTO    CMD_GET_LIGHT_HIGH
+    MOVF UART_RX_BYTE,W
+    XORLW 0x08
+    BTFSC STATUS,2
+    GOTO CMD_GET_LIGHT_HIGH
 
     RETURN
 
